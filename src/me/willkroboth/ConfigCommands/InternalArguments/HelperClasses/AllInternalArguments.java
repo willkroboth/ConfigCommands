cf740e9e48aa38db1ad2ecb8dc36dc5da59d8518
BuildTools <unconfigured@null.spigotmc.org>
2022-04-28 07:20:18 -0400

    Remove InternalVoidArgument from AllInternalArguments' lists

--- src/me/willkroboth/ConfigCommands/InternalArguments/HelperClasses/AllInternalArguments.java
+++ src/me/willkroboth/ConfigCommands/InternalArguments/HelperClasses/AllInternalArguments.java
@@ -3,12 +3,14 @@ package me.willkroboth.ConfigCommands.InternalArguments.HelperClasses;
 import me.willkroboth.ConfigCommands.Functions.NonGenericVarargs.ArgList;
 import me.willkroboth.ConfigCommands.Functions.NonGenericVarargs.NestedArgList;
 import me.willkroboth.ConfigCommands.InternalArguments.InternalArgument;
+import me.willkroboth.ConfigCommands.InternalArguments.InternalVoidArgument;
 
 public final class AllInternalArguments{
     private static final NestedArgList allClasses = new NestedArgList();
     private static final ArgList flatClasses = new ArgList();
 
     public static void addToAllClasses(Class<? extends InternalArgument> clazz){
+        if(clazz.isAssignableFrom(InternalVoidArgument.class)) return;
         ArgList single = new ArgList();
         single.add(clazz);
         allClasses.add(single);
