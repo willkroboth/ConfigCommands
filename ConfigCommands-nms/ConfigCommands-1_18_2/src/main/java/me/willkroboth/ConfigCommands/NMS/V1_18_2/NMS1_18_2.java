b080b14a6aa7836a4d196e897900c5c0703525aa
willkroboth <willkroboth@gmail.com>
2022-07-22 18:40:18 -0400

    Add support for 1.18.2

---
+++ ConfigCommands-nms/ConfigCommands-1_18_2/src/main/java/me/willkroboth/ConfigCommands/NMS/V1_18_2/NMS1_18_2.java
@@ -0,0 +1,13 @@
+package me.willkroboth.ConfigCommands.NMS.V1_18_2;
+
+import me.willkroboth.ConfigCommands.NMS.NMS;
+import me.willkroboth.ConfigCommands.NMS.OpSender;
+import me.willkroboth.ConfigCommands.NMS.V1_18_2.OpSenders1_18_2.OpSender1_18_2;
+import org.bukkit.command.CommandSender;
+
+public class NMS1_18_2 implements NMS {
+    @Override
+    public OpSender makeOpSender(CommandSender sender) {
+        return OpSender1_18_2.makeOpSender(sender);
+    }
+}
--- ConfigCommands-plugin/src/main/java/me/willkroboth/ConfigCommands/NMS/VersionHandler.java
+++ ConfigCommands-plugin/src/main/java/me/willkroboth/ConfigCommands/NMS/VersionHandler.java
@@ -4,13 +4,14 @@ import me.willkroboth.ConfigCommands.Exceptions.UnsupportedVersionException;
 import me.willkroboth.ConfigCommands.NMS.V1_16_5.NMS1_16_5;
 import me.willkroboth.ConfigCommands.NMS.V1_17.NMS1_17;
 import me.willkroboth.ConfigCommands.NMS.V1_18.NMS1_18;
+import me.willkroboth.ConfigCommands.NMS.V1_18_2.NMS1_18_2;
 import me.willkroboth.ConfigCommands.NMS.V1_19.NMS1_19;
 
 public interface VersionHandler {
     static NMS getVersion(String version) {
         return switch (version){
             case "1.19" -> new NMS1_19();
-            case "1.18.2" -> null;
+            case "1.18.2" -> new NMS1_18_2();
             case "1.18", "1.18.1" -> new NMS1_18();
             case "1.17", "1.17.1" -> new NMS1_17();
             case "1.16.5" -> new NMS1_16_5();
