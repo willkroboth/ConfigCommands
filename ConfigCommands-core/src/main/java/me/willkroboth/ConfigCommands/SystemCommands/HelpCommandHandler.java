bafeddf82f3fa2c75370a31586324f25530afa8c
willkroboth <willkroboth@gmail.com>
2022-07-25 12:11:31 -0400

    Make sure help command's help is defined before it is used in the argumentTree

--- ConfigCommands-core/src/main/java/me/willkroboth/ConfigCommands/SystemCommands/HelpCommandHandler.java
+++ ConfigCommands-core/src/main/java/me/willkroboth/ConfigCommands/SystemCommands/HelpCommandHandler.java
@@ -7,26 +7,6 @@ import dev.jorel.commandapi.executors.CommandExecutor;
 import java.util.Arrays;
 
 public class HelpCommandHandler {
-    private static final ArgumentTree argumentTree =
-            ((LiteralArgument) helpBranch("help", HelpCommandHandler.getHelpMessages()))
-                    .withPermission("configcommands.help")
-            .then(helpBranch("build", BuildCommandHandler.getHelpMessages()))
-            .then(helpBranch("debug", DebugCommandHandler.getHelpMessages()))
-            .then(helpBranch("functions", FunctionCommandHandler.getHelpMessages()))
-            .then(helpBranch("reload", ReloadCommandHandler.getHelpMessages()));
-
-    private static ArgumentTree helpBranch(String name, String... messages) {
-        return new LiteralArgument(name).executes(sendMessages(messages));
-    }
-
-    private static CommandExecutor sendMessages(String... messages) {
-        return (sender, args) -> Arrays.stream(messages).forEach(sender::sendMessage);
-    }
-
-    public static ArgumentTree getArgumentTree() {
-        return argumentTree;
-    }
-
     private static final String[] helpMessages = new String[]{
             "Gives information for the different ConfigCommands sections",
             "Usage:",
@@ -35,6 +15,24 @@ public class HelpCommandHandler {
 
     public static String[] getHelpMessages(){
         return helpMessages;
+    }
+    private static final ArgumentTree argumentTree =
+            helpBranch("help", HelpCommandHandler.getHelpMessages()).withPermission("configcommands.help")
+            .then(helpBranch("build", BuildCommandHandler.getHelpMessages()))
+            .then(helpBranch("debug", DebugCommandHandler.getHelpMessages()))
+            .then(helpBranch("functions", FunctionCommandHandler.getHelpMessages()))
+            .then(helpBranch("reload", ReloadCommandHandler.getHelpMessages()));
+
+    private static LiteralArgument helpBranch(String name, String... messages) {
+        return (LiteralArgument) new LiteralArgument(name).executes(sendMessages(messages));
+    }
+
+    private static CommandExecutor sendMessages(String... messages) {
+        return (sender, args) -> Arrays.stream(messages).forEach(sender::sendMessage);
+    }
+
+    public static ArgumentTree getArgumentTree() {
+        return argumentTree;
     }
 
     public static String getShortDescription() {
