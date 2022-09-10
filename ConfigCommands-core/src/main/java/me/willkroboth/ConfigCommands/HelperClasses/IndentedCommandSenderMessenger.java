c93ef094bc4c94b54df943cf809f4e13a5a4f74a
willkroboth <46540330+willkroboth@users.noreply.github.com>
2022-09-10 18:45:42 -0400

    Add class IndentedCommandSenderMessenger and pull shared code with IndentedLogger to abstract class IndentedStringHandler

---
+++ ConfigCommands-core/src/main/java/me/willkroboth/ConfigCommands/HelperClasses/IndentedCommandSenderMessenger.java
@@ -0,0 +1,23 @@
+package me.willkroboth.ConfigCommands.HelperClasses;
+
+import org.bukkit.command.CommandSender;
+
+import java.util.Arrays;
+
+public class IndentedCommandSenderMessenger extends IndentedStringHandler{
+    private final CommandSender sender;
+    private int indentation;
+
+    public IndentedCommandSenderMessenger(CommandSender sender) {
+        super("  ");
+        this.sender = sender;
+    }
+
+    public void sendMessage(String message) {
+        sender.sendMessage(formatMessage(message));
+    }
+
+    public void sendMessage(String... messages) {
+        Arrays.stream(messages).map(this::formatMessage).forEach(sender::sendMessage);
+    }
+}
